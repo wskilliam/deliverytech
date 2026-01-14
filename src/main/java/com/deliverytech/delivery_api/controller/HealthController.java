d29f8321482d2a3dd7ec4d81e87e64f1c251767d
wskilliam <naummoss@gmail.com>
2026-01-14 18:19:32 -0300

    update application, Delivery e a criação controller e HealthController.

---
+++ src/main/java/com/deliverytech/delivery_api/controller/HealthController.java
@@ -0,0 +1,40 @@
+package com.deliverytech.delivery_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public Map<String, String> health(){
+        return Map.of(
+                "status","UP",
+                "timestamp", LocalDateTime.now().toString(),
+                "service", "Delivery API",
+                "java.Version", System.getProperty("java.version")
+        );
+    }
+
+    @GetMapping ("/info")
+    public AppInfo info(){
+      return new AppInfo(
+              "Delivery Tech API",
+              "1.0.0",
+              "[William Rodrigues]",
+              "JDK 21",
+              "Spring Boot 3.2.x"
+      );
+    }
+    //Record para demonstrar recuso do Java 14+ ou 21.
+    public record AppInfo(
+        String application,
+        String version,
+        String developer,
+        String javaVersion,
+        String framework
+    ){}
+}
